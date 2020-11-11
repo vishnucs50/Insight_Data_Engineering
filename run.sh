0ac8dfeca076d1e68b0d90cc91cd4cd58091dde4
Vishnu Kamaraju <77vishnu.k@gmail.com>
2020-11-11 02:11:54 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,8 +3,6 @@
 python ./src/Vishnu.py
 
 # The Program was run by me using the above command. If the above command doesn't work, use the script provided below.
-
-
 #Please note that this command has not been tested by me
 
 # python3.7 ./src/population.py
